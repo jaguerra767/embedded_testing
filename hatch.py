02534916b64e7c7f649a2bdcafb102649de0bba9
Jorge <j.a.guerra@protonmail.com>
2025-07-10 12:13:37 -0700

    added context manager to hatch.py

--- hatch.py
+++ hatch.py
@@ -4,4 +4,5 @@ from demo_gantry.hatch import update_hatch
 HOST = "192.168.1.12"
 PORT = 8888
 controller = ClearCoreController(HOST, PORT)
-update_hatch(controller)
+with controller as cc:
+    update_hatch(cc)
